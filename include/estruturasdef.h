b9f6c661ef042d13db57952ceb67772ffdb1a48d
Nathalia <nathalyabezerra@live.com>
2019-12-13 19:52:05 -0300

    estrutura rgb

---
+++ include/estruturasdef.h
@@ -0,0 +1,14 @@
+#ifndef ESTRUTURASDEF_H_INCLUDED
+#define ESTRUTURASDEF_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{   /*Estrutura criada para ser o tipo da matriz que deve armazenar os pixels da imagem */
+    int r;
+    int g;
+    int b;
+} pix;
+
+#endif // ESTRUTURASDEF_H_INCLUDED
